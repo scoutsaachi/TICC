d3b5f26486cebea7a55c3cc631770f9ac59c04d1
Saachi Jain <saachi@stanford.edu>
2018-02-21 22:09:25 -0800

    added probability generator

--- ticc/motif.py
+++ ticc/motif.py
@@ -1,3 +1,6 @@
+import numpy as np 
+from test_utils import *
+
 class HMM:
     def __init__(self, adjacencyMatrix, llMatrix, initialCosts=None):
         '''
@@ -10,50 +13,60 @@ class HMM:
         self.llMatrix = llMatrix
         self.n = np.shape(llMatrix)[0] # of time steps
         self.m = np.shape(llMatrix)[1] # of clusters
-        self.initialCosts = np.full(m, 0) if initialCosts is None else initialCosts # initial costs
-        self.viterbiGrid = np.zeros((n,m)) # contains probabilities that will be filled by row
-        self.backPointers = np.zeros((n,m)) # contains the back pointers for the viterbi path
+        self.initialCosts = np.full(self.m, 0) if initialCosts is None else initialCosts # initial costs
+        self.viterbiGrid = np.zeros((self.n,self.m)) # contains probabilities that will be filled by row
+        self.backPointers = np.zeros((self.n,self.m)) # contains the back pointers for the viterbi path
         self.backPointers[0].fill(-1)
 
     def UpdateStep(self, ts):
         if ts == 0:
             self.viterbiGrid = self.initialCosts + self.llMatrix[0]
             return
-        prevCosts = np.reshape(self.viterbiGrid[ts - 1], (m,1)) # get the costs from the timestamp before
+        prevCosts = np.reshape(self.viterbiGrid[ts - 1], (self.m,1)) # get the costs from the timestamp before
         withSwitchingCosts = prevCosts + self.adjacencyMatrix
         # we want the min value for each row 
         indices = np.argmin(withSwitchingCosts, axis=0) # backpointers
-        newRow = x[indices, m] + self.llMatrix[ts] # the new ll
+        newRow = x[indices, self.m] + self.llMatrix[ts] # the new ll
         self.viterbiGrid[ts] = newRow
         self.backPointers[ts] = indices
 
 class MotifHMM(HMM):
-    def __init__(self, llMatrix, motif, numStates, beta):
+    def __init__(self, llMatrix, motif, beta):
         '''
         This is the HMM for the motifs
         @params
         motif - a list of IDs that represent the motif, i.e [0,1,0,2]
-        numStates - the number of unique clusters that appear in the motif
         '''
         self.motif = motif
+        numStates = len(motif)
         adjacencyMatrix = self.createAdjacencyMatrix(motif, numStates, beta)
         initCosts = np.full(numStates, np.infty)
         initCosts[0] = 0
-        super.__init__(adjacencyMatrix, llMatrix, initCosts)
+        ll = np.take(llMatrix, motif, axis=1) # grab only the relevant likelihoods
+        HMM.__init__(self, adjacencyMatrix, ll, initCosts)
     
     def createAdjacencyMatrix(self, motif, numStates, beta):
         r = np.full((numStates, numStates), np.infty)
         np.fill_diagonal(r, 0) # costs nothing to go to same cluster
-        np.fill_diagonal(x[:,1:], beta ) # allow transitions to the next state
+        np.fill_diagonal(r[:,1:], beta ) # allow transitions to the next state
         return r
 
 class NullHMM(HMM):
     def __init__(self, llMatrix, beta):
         numStates = np.shape(llMatrix)[1]
         adjacencyMatrix = self.createAdjacencyMatrix(numStates, beta)
-        super.__init__(adjacencyMatrix, llMatrix, None)
+        HMM.__init__(self, adjacencyMatrix, llMatrix, None)
     
     def createAdjacencyMatrix(self, numStates, beta):
         r = np.full((numStates, numStates), beta)
         np.fill_diagonal(r, 0) # no cost to go to same cluster
         return r
+
+def test():
+    breakpoints = [2, 2, 2, 2, 2] # A, B, C
+    numClusters = 4 # A, B, C, D
+    ll, labels = GenerateFakeData(breakpoints, 0.75, 0.05, numClusters)
+    hmm = MotifHMM(ll, np.array([0,1,2]),0)
+
+test()
+
---
+++ ticc/test_utils.py
@@ -0,0 +1,46 @@
+import numpy as np
+
+
+def GenerateFakeData(breakpoints, confidenceMean, confidenceVariance, numClusters):
+    '''
+    Create a probability matrix given the above breakpoints
+    @ params
+    breakpoints - a list of the length of each cluster, with the first and last values being random
+    '''
+    assert len(breakpoints) > 2
+    r = [(-1, -1, -1)]*breakpoints[0]
+    labels = ['_']*breakpoints[0]
+    for i in range(1, len(breakpoints) - 1):
+        identifier = i-1
+        r += [(identifier, confidenceMean, confidenceVariance)] * breakpoints[i]
+        label = chr(ord('A') + identifier)
+        labels += [label]*breakpoints[i]
+    r += [(-1, -1, -1)]*breakpoints[-1]
+    labels += ['_']*breakpoints[0]
+    likelihood = GenerateLikelihoods(r, numClusters)
+    return likelihood, labels
+
+
+def GenerateLikelihoods(assignments, numClusters):
+    '''
+    assignments: a list of (id, probability, variance) tuples. If -1 then garbage
+
+    returns a negative log likelihood matrix
+    '''
+    results = []
+    for c, p, v in assignments:
+        if c == -1:
+            # this is garbage, so just create a random probability
+            a = np.random.rand(numClusters)
+            a = a/np.sum(a)
+            results.append(a)
+        else:
+            a = np.random.rand(numClusters)
+            a[c] = 0
+            val = min(1, np.random.normal(p, v))
+            rest = 1-val
+            a = rest*a/np.sum(a)
+            a[c] = val
+            results.append(a)
+    results = -1*np.log(np.array(results))
+    return results
