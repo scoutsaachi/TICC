288f339c16adf3cf572140c423196b39add4afa5
Saachi Jain <saachi@stanford.edu>
2018-05-16 03:41:28 -0700

    added dataset 2

--- generateDatasets/generate_synthetic.py
+++ generateDatasets/generate_synthetic.py
@@ -27,6 +27,43 @@ def createSegments(assigns, lenSegment):
     return assignment, correctAssignment
 
 
+def createDataset2(outputFilename, outputCorrectName, noiseFraction):
+    num_clusters = 10
+    garbage_clusters = 6
+    clustersequence = [6, 7, 8, 9]
+    numSeqs = 500
+    numGarbage = 10  # number of garbage segments
+    lenSegment = 25  # length of each segment
+    # with probability epsilon, add noise
+    eps = 0.4
+
+    # create assigns
+    assigns = []
+    for _ in range(numSeqs):
+        garbage = np.random.choice(garbage_clusters, numGarbage).tolist()
+        garbage = zip(garbage, [None for _ in len(garbage)])
+        noneVals = None
+        if np.random.random < eps:
+            # pick a random one to edit
+            noneVals = [None for _ in len(clustersequence)]
+            chosenIdx = np.random.choice(len(clustersequence))
+            chosenCluster = np.random.choice(garbage_clusters)
+            noneVals[chosenIdx] = chosenCluster
+        else:  # don't add noise
+            noneVals = [None for _ in len(clustersequence)]
+        assigns += list(zip(clustersequence, noneVals))
+
+    alts = [None for _ in len(assigns)]
+    assigns = list(zip(assigns, alts))
+
+    assignment, correctAssignment = createSegments(assigns, lenSegment)
+    generate_data(num_clusters, NUM_SENSORS, WINDOW_SIZE,
+                  SPARSITY, assignment, outputFilename, RAND_SEED, noiseWeight=noiseFraction)
+
+    correctAssignment = np.array(correctAssignment)
+    np.savetxt(outputCorrectName, correctAssignment, delimiter=",", fmt='%d')
+
+
 def createDataset1(outputFilename, outputCorrectName):
     num_clusters = 10
     garbage_clusters = 6
@@ -41,16 +78,22 @@ def createDataset1(outputFilename, outputCorrectName):
         assigns += np.random.choice(garbage_clusters, numGarbage).tolist()
         assigns += clustersequence
 
+    alts = [None for _ in len(assigns)]
+    assigns = list(zip(assigns, alts))
+
     assignment, correctAssignment = createSegments(assigns, lenSegment)
     generate_data(num_clusters, NUM_SENSORS, WINDOW_SIZE,
                   SPARSITY, assignment, outputFilename, RAND_SEED)
     correctAssignment = np.array(correctAssignment)
     np.savetxt(outputCorrectName, correctAssignment, delimiter=",", fmt='%d')
 
+
 if __name__ == "__main__":
     assert len(sys.argv) == 4
     mode = int(sys.argv[1])
-    mode, output_file, correct_file = int(sys.argv[1]), sys.argv[2], sys.argv[3]
+    mode, output_file, correct_file = int(
+        sys.argv[1]), sys.argv[2], sys.argv[3]
     if mode == 1:
         createDataset1(output_file, correct_file)
-
+    else:
+        createDataset2(output_file, correct_file, 0.5)
--- generateDatasets/generate_synthetic_util.py
+++ generateDatasets/generate_synthetic_util.py
@@ -3,6 +3,7 @@ from snap import GenRndGnm, PNGraph
 
 SAVE_COVS = False
 
+
 def genInvCov(size, low=0.3, upper=0.6, portion=0.2):
     ''' Generate off diagonal blocks'''
     portion = portion/2
@@ -59,13 +60,45 @@ def generate_inverse(rand_seed, n, sparsity, w):
     eigs, _ = np.linalg.eig(inv_matrix)
     return inv_matrix
 
-def generate_data(K, n, w, sparsity, assignments, out_file_name, rand_seed):
+
+def getMeanCov(dataCounter, cluster, cluster_covs, cluster_mean, cluster_mean_stacked, Data, w, n, K):
+    N = n*w
+    if dataCounter == 0:
+        cov_matrix = cluster_covs[cluster][0:n, 0:n]
+        new_mean = cluster_mean_stacked[n * (w-1):N]
+        return new_mean.reshape(n), cov_matrix
+    breakIndex = dataCounter  # update dataCounter
+    # generate the following points
+    num = breakIndex if breakIndex < w else w-1
+    cov_matrix = cluster_covs[cluster][0:(num+1)*n, 0:(num+1)*n]
+    Sig22 = cov_matrix[(num)*n:(num+1)*n, (num)*n:(num+1)*n]
+    Sig11 = cov_matrix[0:(num)*n, 0:(num)*n]
+    Sig21 = cov_matrix[(num)*n:(num+1)*n, 0:(num)*n]
+    Sig12 = Sig21.T
+    # sigma2|1
+    sig21_invsig11 = np.dot(Sig21, np.linalg.inv(Sig11))
+    cov_mat_tom = Sig22 - np.dot(sig21_invsig11, Sig12)
+    a = np.zeros((num*n, 1))
+    for idx in range(num):
+        if breakIndex < w:
+            a[idx*n:(idx+1)*n, 0] = Data[idx, :].reshape((n))
+        else:
+            a[idx*n:(idx+1)*n, 0] = Data[breakIndex -
+                                         w + 1 + idx, :].reshape([n])
+    new_mean = cluster_mean + \
+        np.dot(sig21_invsig11, (a - cluster_mean_stacked[0:(num)*n, :]))
+    return new_mean.reshape(n), cov_mat_tom
+
+
+def generate_data(K, n, w, sparsity, assignments, out_file_name, rand_seed, noiseWeight=1):
     '''
     K: number of clusters
     n: number of sensors
     w: window size
-    assignments: [(clusterID, numberOfPoints)]
+    assignments: [(cluseterID1, clusterID2), numberOfPoints)]
+        if clusterID2 = None, then we're not adding noise
     out_file_name: outputfile to save assignments
+    noiseWeight: how much weight should clusterID2 get
     '''
 
     # GENERATE POINTS
@@ -89,39 +122,22 @@ def generate_data(K, n, w, sparsity, assignments, out_file_name, rand_seed):
     T = sum([val[1] for val in assignments])
     Data = np.zeros((T, n))
     dataCounter = 0
-    for cluster, numPoints in assignments:
+    for clusterList, numPoints in assignments:
+        cluster1, cluster2 = clusterList
         for i in range(numPoints):
-            if dataCounter == 0:
-                cov_matrix = cluster_covs[cluster][0:n, 0:n]
-                new_mean = cluster_mean_stacked[n * (w-1):N]
-                new_row = np.random.multivariate_normal(
-                new_mean.reshape(n), cov_matrix)
-                Data[0, :] = new_row
-                dataCounter += 1
-                continue
-            breakIndex = dataCounter  # update dataCounter
+            mean, cov = None, None
+            mean1, cov1 = getMeanCov(dataCounter, cluster1, cluster_covs,
+                       cluster_mean, cluster_mean_stacked, Data, w, n, K)
+            if cluster2 is None:
+                mean, cov = mean1, cov1 
+            else:
+                mean2, cov2 = getMeanCov(dataCounter, cluster2, cluster_covs,
+                       cluster_mean, cluster_mean_stacked, Data, w, n, K)
+                mean = (1-noiseWeight)*mean1 + noiseWeight*mean2
+                cov = (1-noiseWeight)*cov1 + noiseWeight*cov2
+            new_row = np.random.multivariate_normal(mean, cov)
+            Data[dataCounter, :] = new_row
             dataCounter += 1
-            # generate the following points
-            num = breakIndex if breakIndex < w else w-1
-            cov_matrix = cluster_covs[cluster][0:(num+1)*n, 0:(num+1)*n]
-            Sig22 = cov_matrix[(num)*n:(num+1)*n, (num)*n:(num+1)*n]
-            Sig11 = cov_matrix[0:(num)*n, 0:(num)*n]
-            Sig21 = cov_matrix[(num)*n:(num+1)*n, 0:(num)*n]
-            Sig12 = Sig21.T
-            # sigma2|1
-            sig21_invsig11 = np.dot(Sig21, np.linalg.inv(Sig11))
-            cov_mat_tom = Sig22 - np.dot(sig21_invsig11, Sig12)
-            a = np.zeros((num*n, 1))
-            for idx in range(num):
-                if breakIndex < w:
-                    a[idx*n:(idx+1)*n, 0] = Data[idx, :].reshape((n))
-                else:
-                    a[idx*n:(idx+1)*n, 0] = Data[breakIndex -
-                                                 w + 1 + idx, :].reshape([n])
-            new_mean = cluster_mean + np.dot(sig21_invsig11, (a - cluster_mean_stacked[0:(num)*n, :]))
-            new_row = np.random.multivariate_normal(
-                new_mean.reshape(n), cov_mat_tom)
-            Data[breakIndex, :] = new_row
 
     print("length of generated Data is:", Data.shape[0])
     np.savetxt(out_file_name, Data, delimiter=",", fmt='%1.4f')
