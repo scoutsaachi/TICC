e67c08bf5ebb19e6bd802583de048d16e7de7312
Saachi Jain <saachi@stanford.edu>
2018-05-31 20:25:49 -0700

    changes to synth plot

--- syntheticPlot.py
+++ syntheticPlot.py
@@ -7,12 +7,12 @@ print("Started")
 EPSILONS = np.arange(0, 0.8, 0.1).tolist()
 
 mapping = {
-    'casc06': ('0.6/assign.out', "CASC $\gamma$ = 0.6", "-ro"),
-    'casc99': ('0.99/assign.out', "CASC, $\gamma$ = 0.99", "-ms"),
-    'ticc': ('old/assign.out', "TICC", "-.bd"),
+    'casc06': ('0.6/assign.out', "CASC $\gamma$ = 0.6", "-bv"),
+    # 'casc99': ('0.99/assign.out', "CASC, $\gamma$ = 0.99", "-ms"),
+    'ticc': ('old/assign.out', "TICC", "--ro"),
     'kmeans': ('kmeans.out', "KMEANS", ":k*"),
-    'gmm': ('gmm.out', "GMM", ":g1"),
-    'hmm': ('hmm.out', "HMM", ":c2"),
+    'gmm': ('gmm.out', "GMM", ":gd"),
+    'hmm': ('hmm.out', "HMM", ":ms"),
 }
 
 def getScores(directory):
@@ -42,24 +42,27 @@ def loadAndPlotScores(directory):
     labels = EPSILONS
     titles = ["Weighted Macro F1 Score on Motif Clusters", 
         "Weighted Macro F1 Score on All Clusters",
-        "Accuracy Score on Motif Segments"]
+        "Accuracy Score on Motif Segments vs $\epsilon$"]
     for i in range(3):
-        plt.figure(i, figsize=(6, 7))
+        plt.figure(i, figsize=(6, 6))
         ax = plt.subplot(111)
         markerCount = 0
         for k,v in scoreValues.items():
             plt.plot(labels, v[i][:len(labels)], mapping[k][2], label=mapping[k][1])
+            string = ["%0.3f " % u for u in v[i][:len(labels)] ]
+            print(titles[i], k, ' & '.join(string))
             markerCount += 1
         box = ax.get_position()
-        ax.set_position([box.x0, box.y0 + box.height * 0.15,
-                 box.width, box.height * 0.85])
-        ax.legend(loc='upper center', bbox_to_anchor=(0.5, -0.14),
-          fancybox=True, shadow=True, ncol=3)
-        plt.xlabel("Fraction of noisy segments")
-        plt.ylim(ymin=0)
-        plt.title(titles[i])
+        ax.legend(loc='lower center', ncol=3, fancybox=False, edgecolor="black")
+        # ax.set_position([box.x0, box.y0 + box.height * 0.15,
+        #          box.width, box.height * 0.85])
+        # ax.legend(loc='upper center', bbox_to_anchor=(0.5, -0.14),
+        #   fancybox=True, shadow=True, ncol=3)
+        plt.xlabel("$\epsilon$")
+        plt.ylim(ymin=0, ymax=1)
+        # plt.title(titles[i])
         if i == 2:
-            plt.ylabel("Accuracy score on motif segments vs Noise")
+            plt.ylabel("Accuracy score on motif segments")
         else:
             plt.ylabel("Weighted Macro F1 Score")
         # plt.legend()
